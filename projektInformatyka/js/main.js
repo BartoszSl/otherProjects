3ce17960d0dbe96d9a28ca523b6c1282bc76e2b8
BartoszSl <github.bartek@gmail.com>
2023-04-29 22:38:41 +0200

    JavaScript

---
+++ projektInformatyka/js/main.js
@@ -0,0 +1,42 @@
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const navTitle = document.querySelector('.nav__title')
+const header = document.querySelector('.header')
+
+const handleNav = () => {
+    nav.classList.toggle('nav--active')
+
+    navBtnBars.classList.remove('black-bars-color');
+
+    allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+            nav.classList.remove('nav--active')
+        })
+    })
+
+    handleNavItemsAnimation();
+}
+
+const handleNavItemsAnimation = () => {
+    let delayTime = 0;
+
+    allNavItems.forEach(item => {
+        item.classList.toggle('nav-items-animation')
+        item.style.animationDelay = '.' + delayTime + 's';
+        delayTime++;
+    })
+}
+
+
+const changeVisibilityTitle = (e) => {
+    if (window.scrollY >= header.clientHeight - 50) {
+        navTitle.classList.add('visibility')
+    } else {
+        navTitle.classList.remove('visibility')
+    }
+}
+
+navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', changeVisibilityTitle)
