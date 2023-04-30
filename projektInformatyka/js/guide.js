1e84e51dd88872fae3a3fbd15d3c91ec0aa9bcbe
BartoszSl <github.bartek@gmail.com>
2023-04-30 23:09:55 +0200

    guide to do Full Page

---
+++ projektInformatyka/js/guide.js
@@ -0,0 +1,57 @@
+const title = [
+	'Binkowski',
+	'Stadion',
+	'Kadzielnia',
+	'Skwer Harcerski',
+	'Park',
+	'Muzeum w kielcach',
+	'McDonald',
+	'Zalew Strawczyn',
+	'Basen Strawczyn',
+	'Kino Strawczyn',
+	'ZOO w lisowie',
+];
+
+const text = [
+	'Baseny Tropikalne Binkowski to idealne miejsce do spędzenia wolnego czasu samotnie , bądź z rodziną lub przyjaciółmi. Obiekt składa się z M.in jednego dużego basenu z barem wodnym, basenu dla dzieci, rzeki prowadzącej przez cały obiekt. Budynek jest cały czas wzbogacany o nowe atrakcje, a turyści z innych miejsc mogą zatrzymać się na nocleg w hotelu znajdującym się obok. ',
+	'Stadion Leśny to kompleks parkowo-leśny położony w południowej części Kielc, w pobliżu Pakosza i Baranówka. Miejsce to jest idealne na długie spacery, bądź inne aktywności na świeżym powietrzu.',
+	'Rezerwat Przyrody Kadzielnia to miejsce, w którym każdy znajdzie coś dla siebie. Na terenie obiektu znajduje się M.injaskinia, tyrolka, oraz amfiteatr. Rezerwat Przyrody Kadzielnia to miejsce, w którym każdy znajdzie coś dla siebie. Na terenietego obiektu znajduje się M.in jaskinia, tyrolka, oraz amfiteatr. Rezerwat Przyrody Kadzielnia to miejsce, w którymkażdy znajdzie coś dla siebie. Na terenie tego obiektu znajduje się M.in jaskinia, tyrolka, oraz amfiteatr.',
+	'Jest to plac położony koło centrum miasta. Na jego terenie znajdują się ciągi alejek spacerowych, place zabaw oraz przebiega przez niego (wzdłuż rzeki) ścieżka rowerowa. Na skwerze stoi pomnik poświęcony "Harcerzom poległym za Ojczyznę" autorstwa Stefana Dulnego.Przez skwer przechodzą również różne szlaki turystyczne prowadzące przez zabytkowe i ciekawe turystycznie miejsca. ',
+	'Jeden z najstarszych parków w Polsce, leżący w centrum miasta. W zachodniej części parku znajduje się staw określany mianem "podzamecki", mający na środku fontannę z ozdobnymi dyszami w kształcie ryb. Na terenie parku organizowane są co jakiś czas koncerty i uroczystości.',
+	'Jest to placówka muzealna z ponad 100-letnią tradycją. W swoich zbiorach posiada cenne eksponaty z dziedziny malarstwa, rzemiosła artystycznego, sztuki ludowej, archeologii i przyrody.',
+	'Jest to amerykańska sieć barów szybkiej obsługi, sprzedająca głównie burgery, frytki i napoje. Można w niej miło spędzić czas ze znajomymi. Często organizowane są tam różne wydarzenia, przykładowo: limitowany zestaw artysty Mata.',
+	'Zalew w Strawczynie ma powierzchnię blisko 10 ha i powstał przez zagrodzenie tamą cieku wodnego o nazwie Olszówka (znanego również jako Trupień). Zbiornik ma charakter wypoczynkowo-rekreacyjny i jest bardzo dobrze zagospodarowany turystycznie.',
+	'Duży basen z zjeżdżalnia i wieloma różnymi strefami do pływania takie jak jacuzzi, sztuczna rzeka i wiele więcej. Do tego bardzo tanie bilety bo tylko 8 zł za 1h',
+	'Małe bardzo nowoczesne kino które powstało w tym roku. Jest to jedne kino w całej gminie. Co prawda nie ma tam najnowszych filmów ale napewno są te najciekawsze. Bilety wstępu tak samo jak na basen są w bardzo niskiej cenie',
+	'Zoo „Leśne zacisze” to miejsce idealne do spędzenia wolnego czasu na świeżym powietrzu. Znajduje się w nim około 100 gatunków zwierząt z 6 kontynentów!',
+];
+
+const dataHref = [
+	'Binkowski',
+	'Stadion',
+	'./img/kadzielnia.png',
+	'Skwer Harcerski',
+	'Park',
+	'Muzeum w kielcach',
+	'McDonald',
+	'Zalew Strawczyn',
+	'Basen Strawczyn',
+	'Kino Strawczyn',
+	'ZOO w lisowie',
+];
+
+const mapaHref = [
+	'Binkowski',
+	'Stadion',
+	'./img/kadzielnia.png',
+	'Skwer Harcerski',
+	'Park',
+	'Muzeum w kielcach',
+	'McDonald',
+	'Zalew Strawczyn',
+	'Basen Strawczyn',
+	'Kino Strawczyn',
+	'ZOO w lisowie',
+];
+
+console.log(mapaHref[1]);
