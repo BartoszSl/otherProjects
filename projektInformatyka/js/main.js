38c6d335b6a81a84de8c9c1776d8034d6b3a4da8
BartoszSl <github.bartek@gmail.com>
2023-05-01 18:07:55 +0200

    main Full Script

--- projektInformatyka/js/main.js
+++ projektInformatyka/js/main.js
@@ -4,6 +4,7 @@ const allNavItems = document.querySelectorAll('.nav__item')
 const navBtnBars = document.querySelector('.burger-btn__bars')
 const navTitle = document.querySelector('.nav__title')
 const header = document.querySelector('.header')
+const allSections = document.querySelectorAll('.section')
 
 const handleNav = () => {
     nav.classList.toggle('nav--active')
@@ -31,12 +32,26 @@ const handleNavItemsAnimation = () => {
 
 
 const changeVisibilityTitle = (e) => {
-    if (window.scrollY >= header.clientHeight - 50) {
+    if (window.scrollY >= header.clientHeight - 100) {
         navTitle.classList.add('visibility')
     } else {
         navTitle.classList.remove('visibility')
     }
 }
 
+const handleObserver = () => {
+    const currentSection = window.scrollY;
+
+    allSections.forEach(section => {
+
+        if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.add('black-bars-color')
+        } else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.remove('black-bars-color')
+        }
+    })
+}
+
 navBtn.addEventListener('click', handleNav);
 window.addEventListener('scroll', changeVisibilityTitle)
+window.addEventListener('scroll', handleObserver)
